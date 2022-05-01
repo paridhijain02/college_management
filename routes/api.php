5ed542d099068d3fae28b9f6dc1c85590ead882f
paridhijain02 <46786103+paridhijain02@users.noreply.github.com>
2022-05-02 00:52:40 +0530

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,15 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/sview','ApiController@studentView')->name('student.view');
-Route::get('/tview','ApiController@teacherView')->name('teacher.view');
-Route::get('/aview','ApiController@allView')->name('all.view');
+Route::get('/studentView','ApiController@studentView')->name('student.view');
+Route::get('/teacherView','ApiController@teacherView')->name('teacher.view');
+
+//Route::middleware(['api'])->group(function($router) 
+//{
+Route::get('/allPeopleView','ApiController@allView')->name('all.view');
+//})->middleware('tea_guard');
--- routes/web.php
+++ routes/web.php
@@ -1,77 +1,65 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Route::get('/','ProjectController@welcome')->name('register.welcome');
 
-Route::get('/slogin','ProjectController@studentLogin')->name('register.slogin');
-Route::post('/slogin','ProjectController@studentPostLogin');
+Route::get('/studentLogin','StudentController@studentLogin')->name('register.studentLogin');
+Route::post('/studentLogin','StudentController@studentPosteacherLogin');
 
-Route::get('/tlogin','ProjectController@teacherLogin')->name('register.tlogin');
-Route::post('/tlogin','ProjectController@teacherPostLogin');
+Route::get('/teacherLogin','TeacherController@teacherLogin')->name('register.teacherLogin');
+Route::post('/teacherLogin','TeacherController@teacherPosteacherLogin');
 
-Route::get('/alogin','ProjectController@adminLogin')->name('register.alogin');
-Route::post('/alogin','ProjectController@adminPostLogin');
+Route::get('/adminLogin','AdminController@adminLogin')->name('register.adminLogin');
+Route::post('/adminLogin','AdminController@adminPosteacherLogin');
 
-Route::get('/ssignup','ProjectController@studentRegistered')->name('sregister.signup');
-Route::post('/ssignups','ProjectController@studentStore');
+Route::get('/studentSignup','StudentController@studentRegistered')->name('sregister.signup');
+Route::post('/studentSignups','StudentController@studentStore');
 
-Route::get('/tsignup','ProjectController@teacherRegistered')->name('tregister.signup');
-Route::post('/tsignups','ProjectController@teacherStore');
-
-// Route::get('/sview','ProjectController@studentView')->name('student.view');
-// Route::get('/tview','ProjectController@teacherView')->name('teacher.view');
+Route::get('/teacherSignup','TeacherController@teacherRegistered')->name('tregister.signup');
+Route::post('/teacherSignups','TeacherController@teacherStore');
 
 Route::get('/notexist','ProjectController@notExist');
 Route::get('/notloggedin','ProjectController@notLoggedIn');
 
 Route::group(['middleware'=>'stu_guard'],function()
 {
-    Route::any('/sprofilee', 'ProjectController@studentProfile');
-    Route::get('/sprofilee/assignment_write/{id}','ProjectController@assignmentWrite');
-    Route::get('/student_assignment_view','ProjectController@studentAssignmentToTeacher');
+    Route::any('/studentProfile', 'StudentController@studentProfile');
+    Route::get('/studentProfile/assignment_write/{id}','StudentController@assignmentWrite');
+    Route::get('/student_assignment_view','TeacherController@studentAssignmentToTeacher');
 });
 
 Route::group(['middleware'=>'tea_guard'],function()
 {
-    Route::any('/tprofilee', 'ProjectController@teacherProfile');
-    Route::get('/tprofilee/s_edit/{id}','ProjectController@studentEdit');
-    Route::get('/tprofilee/t_edit/{id}','ProjectController@teacherEdit');
-    Route::get('/create_assignment','ProjectController@createNewAssignment');
-    Route::get('/my_assignments','ProjectController@teacherMyAsssignment');
-    Route::get('/assignments_delete/{id}','ProjectController@assignmentDelete');
+    Route::any('/teacherProfile', 'TeacherController@teacherProfile');
+    Route::patch('/teacherProfile/studentEdit/{id}','TeacherController@studentEdit');
+    Route::patch('/teacherProfile/teacherEdit/{id}','TeacherController@teacherEdit');
+    Route::get('/create_assignment','TeacherController@createNewAssignment');
+    Route::get('/my_assignments','TeacherController@teacherMyAsssignment');
+    Route::get('/assignmentstudentDelete/{id}','TeacherController@assignmentDelete');
 });
 
 Route::group(['middleware'=>'adm_guard'],function()
 {
-    Route::any('/aprofilee', 'ProjectController@adminProfile');
-    Route::get('/aprofilee/s_edit/{id}','ProjectController@studentEditbyadmin');
-    Route::get('/aprofilee/t_edit/{id}','ProjectController@teacherEditbyadmin');
+    Route::any('/adminProfile', 'AdminController@adminProfile');
+    Route::get('/adminProfile/studentEdit/{id}','AdminController@studentEditbyadmin');
+    Route::get('/adminProfile/teacherEdit/{id}','AdminController@teacherEditbyadmin');
 });
 
-Route::get('/tprofilee/s_delete/{id}','ProjectController@studentDelete');
-Route::post('/tprofilee/s_update/{id}','ProjectController@studentUpdate');
+//Route::get('/teacherProfile/studentDelete/{id}','TeacherController@studentDelete');
+Route::delete('/teacherProfile/studentDelete/{id}','TeacherController@studentDelete');
+Route::post('/teacherProfile/studentUpdate/{id}','TeacherController@studentUpdate');
 
-Route::get('/tprofilee/t_delete/{id}','ProjectController@teacherDelete');
-Route::post('/tprofilee/t_update/{id}','ProjectController@teacherUpdate');
+Route::delete('/teacherProfile/teacherDelete/{id}','TeacherController@teacherDelete');
+Route::post('/teacherProfile/teacherUpdate/{id}','TeacherController@teacherUpdate');
 
-Route::get('/aprofilee/s_delete/{id}','ProjectController@studentDeletebyadmin');
-Route::post('/aprofilee/s_update/{id}','ProjectController@studentUpdatebyadmin');
+Route::delete('/adminProfile/studentDelete/{id}','AdminController@studentDeletebyadmin');
+Route::post('/adminProfile/studentUpdate/{id}','AdminController@studentUpdatebyadmin');
 
-Route::get('/aprofilee/t_delete/{id}','ProjectController@teacherDeletebyadmin');
-Route::post('/aprofilee/t_update/{id}','ProjectController@teacherUpdatebyadmin');
+Route::delete('/adminProfile/teacherDelete/{id}','AdminController@teacherDeletebyadmin');
+Route::post('/adminProfile/teacherUpdate/{id}','AdminController@teacherUpdatebyadmin');
 
-Route::post('/create_assignment','ProjectController@createNewAssignmentPost');
-Route::post('/sprofilee/assignment_write_post/{id}','ProjectController@assignmentWritePost');
+Route::post('/create_assignment','TeacherController@createNewAssignmentPost');
+Route::post('/studentProfile/assignment_write_post/{id}','StudentController@assignmentWritePost');
 
-Route::get('/slogout','ProjectController@studentlogout');
-Route::get('/tlogout','ProjectController@teacherlogout');
-Route::get('/alogout','ProjectController@adminlogout');
+Route::get('/studentLogout','StudentController@studentLogout');
+Route::get('/teacherLogout','TeacherController@teacherlogout');
+Route::get('/adminLogout','AdminController@adminlogout');
